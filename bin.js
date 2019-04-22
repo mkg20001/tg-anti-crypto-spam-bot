31298ed74d5b70c880d4cd5c2199893d19436e64
Maciej Krüger <mkg20001@gmail.com>
2019-04-23 00:56:10 +0200

    feat: finish the bot for today

--- bin.js
+++ bin.js
@@ -5,6 +5,8 @@
 const os = require('os')
 const path = require('path')
 const fs = require('fs')
+const crypto = require('crypto')
+const sha2 = (str) => crypto.createHash('sha256').update(str).digest('hex')
 
 const TeleBot = require('telebot')
 const bot = new TeleBot(process.argv[2])
@@ -24,9 +26,12 @@ const log = pino({name: 'tg-crypto-spam-bot'})
 const wordlist = []
 const {scores, actions} = config
 
+const db = fs.createWriteStream('captures.json', {flags: 'a'})
+const collect = (data) => db.write(JSON.stringify(data) + '\n')
+
 for (const group in config.wordlists) {
   let {score, words} = config.wordlists[group]
-  words.forEach(word => wordlist.push({word, group, score})
+  words.forEach(word => wordlist.push({word, group, score}))
 }
 
 const origOn = bot.on.bind(bot)
@@ -52,22 +57,34 @@ bot.on(['/start', '/hello'], (msg) => msg.reply.text('This bot helps you to get 
 bot.on('text', (msg) => {
   console.log(msg)
   
+  if (msg.type === 'private') {
+    return
+  }
+  
   let urls = []
   let words = msg.text.match(/\w+/gmi).map(w => w.toLowerCase())
   let hasTGUrl = false // t.me/joinchat, etc
   
-  let forbiddenWords = wordlist.filter({word} => words.indexOf(word) !== -1)
+  let forbiddenWords = wordlist.filter(({word}) => words.indexOf(word) !== -1)
   
   let score = forbiddenWords.reduce((a, b) => a + b.score, 0) +
     (scores.isABot * msg.from.is_bot) +
     (scores.url * urls.length) +
     (scores.tgUrl * hasTGUrl)
-    
-  if (actions.ban <= score) {
-    // TODO: impl
-  } else if (actions.kick <= score) {
-    // TODO: impl
+
+  // msg.reply.text(JSON.stringify({urls, words, hasTGUrl, score}))
+
+  if (actions.del <= score) {
+    let uniq = sha2(msg.chat_id + '@' + msg.message_id)
+    msg.reply.text(`Deleted crypto spam ${uniq} - Beta, complain with this ID in your issues if this was a mistake`)
+    bot.deleteMessage(msg.chat.id, msg.message_id)
+    collect({score, text: msg.text, data: {words, urls, forbiddenWords, hasTGUrl}, id: uniq})
   }
+  
+  if (actions.kick <= score) {
+  } else if (actions.ban <= score) {
+  }
+  
 })
 
 bot.start()
