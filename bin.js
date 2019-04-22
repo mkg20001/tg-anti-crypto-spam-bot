1752635699d5163ab18f0147bf9541ae24be166f
Maciej Krüger <mkg20001@gmail.com>
2019-04-23 00:39:03 +0200

    feat: initial version

--- bin.js
+++ bin.js
@@ -13,13 +13,21 @@ const URI = require('urijs')
 const ERROR_REPLY = 'Boom!\nYou just made this bot go kaboom!\nHave a :cookie:!'
 
 const yaml = require('js-yaml')
-const words = yaml.safeLoad(String(fs.readFileSync(path.join(__dirname, 'words.yaml'))))
+const config = yaml.safeLoad(String(fs.readFileSync(path.join(__dirname, 'config.yaml'))))
 
 const Sentry = require('@sentry/node')
 Sentry.init({dsn: process.env.SENTRY_DSN})
 
 const pino = require('pino')
 const log = pino({name: 'tg-crypto-spam-bot'})
+
+const wordlist = []
+const {scores, actions} = config
+
+for (const group in config.wordlists) {
+  let {score, words} = config.wordlists[group]
+  words.forEach(word => wordlist.push({word, group, score})
+}
 
 const origOn = bot.on.bind(bot)
 bot.on = (ev, fnc, ...a) => {
@@ -40,5 +48,26 @@ bot.on = (ev, fnc, ...a) => {
 }
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('This bot helps you to get rid of crypto spam in your group!', {webPreview: false}))
+
+bot.on('text', (msg) => {
+  console.log(msg)
+  
+  let urls = []
+  let words = msg.text.match(/\w+/gmi).map(w => w.toLowerCase())
+  let hasTGUrl = false // t.me/joinchat, etc
+  
+  let forbiddenWords = wordlist.filter({word} => words.indexOf(word) !== -1)
+  
+  let score = forbiddenWords.reduce((a, b) => a + b.score, 0) +
+    (scores.isABot * msg.from.is_bot) +
+    (scores.url * urls.length) +
+    (scores.tgUrl * hasTGUrl)
+    
+  if (actions.ban <= score) {
+    // TODO: impl
+  } else if (actions.kick <= score) {
+    // TODO: impl
+  }
+})
 
 bot.start()
