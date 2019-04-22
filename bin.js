75a5064c2a99431817d4110294b915192add1410
Maciej Krüger <mkg20001@gmail.com>
2019-04-23 01:56:55 +0200

    feat: handle forwarded msgs and urls

--- bin.js
+++ bin.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint-disable guard-for-in */
+
 'use strict'
 
 const path = require('path')
@@ -21,6 +23,7 @@ const pino = require('pino')
 const log = pino({name: 'tg-crypto-spam-bot'})
 
 const wordlist = []
+const urllist = []
 const {scores, actions} = config
 
 const db = fs.createWriteStream(process.env.CAPTURE_FILE || 'captures.json', {flags: 'a'})
@@ -29,6 +32,11 @@ const collect = (data) => db.write(JSON.stringify(data) + '\n')
 for (const group in config.wordlists) {
   let {score, words} = config.wordlists[group]
   words.forEach(word => wordlist.push({word, group, score}))
+}
+
+for (const group in config.urllists) {
+  let {score, domains} = config.urllists[group]
+  domains.forEach(domain => urllist.push({domain, group, score}))
 }
 
 const origOn = bot.on.bind(bot)
@@ -46,36 +54,43 @@ bot.on = (ev, fnc, ...a) => {
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('This bot helps you to get rid of crypto spam in your group!', {webPreview: false}))
 
-bot.on('text', (msg) => {
-  console.log(msg)
-
+const handle = (msg) => {
   if (msg.type === 'private') {
     return
   }
 
-  let urls = msg.text.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gmi)
+  let urls = msg.text.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gmi) || []
   let words = msg.text.match(/\w+/gmi).map(w => w.toLowerCase())
   let hasTGUrl = msg.text.indexOf('t.me/joinchat') !== -1
 
   let forbiddenWords = wordlist.filter(({word}) => words.indexOf(word) !== -1)
+  let hostnamesOnly = urls.map(url => new URL(url).hostname)
+  let forbiddenUrls = urllist.filter(({domain}) => hostnamesOnly.indexOf(domain) !== -1)
 
   let score = forbiddenWords.reduce((a, b) => a + b.score, 0) +
+    forbiddenUrls.reduce((a, b) => a + b.score, 0) +
     (scores.isABot * msg.from.is_bot) +
-    (scores.url * (urls || []).length) +
+    (scores.url * urls.length) +
     (scores.tgUrl * hasTGUrl)
 
-  // msg.reply.text(JSON.stringify({urls, words, hasTGUrl, score}))
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(msg) // eslint-disable-line no-console
+    msg.reply.text(JSON.stringify({urls, words, hasTGUrl, score}))
+  }
 
   if (actions.del <= score) {
     let uniq = sha2(msg.chat_id + '@' + msg.message_id)
-    msg.reply.text(`Deleted crypto spam because score ${score} - If this was a mistake then create an issue here https://github.com/mkg20001/tg-anti-crypto-spam-bot/issues/new?title=[wrongful%20deletion]%20${uniq}`)
+    msg.reply.text(`Deleted crypto spam because score ${score} - If this was a mistake then create an issue here https://github.com/mkg20001/tg-anti-crypto-spam-bot/issues/new?title=[wrongful%20deletion]%20${uniq}`, {render_web: false})
     bot.deleteMessage(msg.chat.id, msg.message_id)
-    collect({score, text: msg.text, data: {words, urls, forbiddenWords, hasTGUrl}, id: uniq})
+    collect({score, text: msg.text, data: {words, urls, forbiddenWords, forbiddenUrls, hasTGUrl}, id: uniq})
   }
 
   if (actions.ban <= score) {
   } else if (actions.kick <= score) {
   }
-})
+}
+
+bot.on('forward', handle)
+bot.on('text', handle)
 
 bot.start()
